8b49c42f7d3d9bf14205f86a0a6d86f706d1faf0
rodsef <ramirezrodsef@gmail.com>
2025-04-17 14:42:50 +0800

    added profile page and event info page design

--- lib/pages/event_info_page.dart
+++ lib/pages/event_info_page.dart
@@ -9,81 +9,121 @@ class EventInfoPage extends StatelessWidget {
       body: SafeArea(
         child: ListView(
           children: [
-            ListTile(title: Text("Event Info Page")),
-
             ListTile(
               title: Center(
                 child: Container(
-                height: 300,
-                width: 300,
-                color: Colors.grey,
-                child: Center(child: Text("IMG"),
-                )
+                  height: 300,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.grey,
+                  ),
+                  child: Center(child: Text("IMG")),
+                ),
               ),
-              )
             ),
 
             ListTile(
               //Event Info
-              title: Text("Event Title"),
-              subtitle: Container(
+              title: Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Color(0xFF1A2C54),
+                  borderRadius: BorderRadius.circular(20),
+                ),
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Container(
-                      child: Column(
-                        children: [
-                          Text("Monday",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold
-                          ),),
-
-
-                          Text("6:00 - 5:00"),
-                          Text("March 21, 2025"),
-
-
-                          Text("Organized by: ",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold
-                          ),),
-                          Text("Organization name"),
-
-
-                          Text("Participants",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold
-                          ),),
-                          Text("CCS"),
-                          Text("COA"),
-                          Text("COB"),
-                        ],
+                    Text(
+                      "Event Title",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
                       ),
                     ),
-                    
+
                     Container(
-                      padding: EdgeInsets.all(20),
-                      decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(20),
-                      ),
                       child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text("Event Details",
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold
-                          )),
                           Text(
-                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                            "Monday",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+
+                          Text(
+                            "6:00 - 5:00",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          Text(
+                            "March 21, 2025",
+                            style: TextStyle(color: Colors.white),
+                          ),
+
+                          SizedBox(height: 20),
+                          Text(
+                            "Organized by: ",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Text(
+                            "Organization name",
+                            style: TextStyle(color: Colors.white),
+                          ),
+
+                          SizedBox(height: 20),
+                          Text(
+                            "Participants",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Text("CCS", style: TextStyle(color: Colors.white)),
+                          Text("COA", style: TextStyle(color: Colors.white)),
+                          Text("COB", style: TextStyle(color: Colors.white)),
+
+                          SizedBox(height: 20),
+                          Container(
+                            padding: EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                              color: Color(0xFF0A1D34),
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Column(
+                              children: [
+                                Text(
+                                  "Event Details",
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Text(
+                                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ],
+                            ),
+                          ),
+
+                          SizedBox(height: 20),
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.pushNamed(context, '/receipt_page');
+                            },
+                            child: Text("Register for Event"),
                           ),
                         ],
                       ),
                     ),
-                    ElevatedButton(onPressed: (){
-                      Navigator.pushNamed(context, '/receipt_page');
-
-                    }, 
-                    child: Text("Register for Event"))
                   ],
                 ),
               ),
--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -6,185 +6,276 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-          child: ListView(
-            children: [
-              ListTile(
-                title: Container(
-                  height: 100,
-                  width: 100,
+      body: Center(
+        child: ListView(
+          children: [
+            ListTile(
+              title: Align(
+                child: Container(
+                  height: 300,
+                  width: 300,
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(100),
+                    borderRadius: BorderRadius.circular(999),
                     color: Colors.grey,
                   ),
                 ),
               ),
-              ListTile(
-                title: Text("@Username", style: TextStyle(fontSize: 40)),
-              ),
-
-              ListTile(
-                title: Container(
-                  padding: EdgeInsets.all(0),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(40),
-                    border: Border.all(color: Colors.black),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          "Full Name",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                        decoration: BoxDecoration(color: Colors.blue),
+            ),
+            ListTile(
+              title: Align(
+                alignment: Alignment.center,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      "@username",
+                      style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
                       ),
-                      SizedBox(width: 40),
-                      Text("Di O. Nela"),
-                      SizedBox(width: 40),
-                      ElevatedButton(onPressed: () {}, child: Text("Edit")),
-                    ],
-                  ),
+                    ),
+                    SizedBox(width: 20),
+                    ElevatedButton(onPressed: () {}, child: Icon(Icons.copy)),
+                  ],
                 ),
               ),
+            ),
 
-              ListTile(
-                title: Container(
-                  padding: EdgeInsets.all(0),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(40),
-                    border: Border.all(color: Colors.black),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          "Program",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                        decoration: BoxDecoration(color: Colors.blue),
+            ListTile(
+              title: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(40),
+                  border: Border.all(color: Colors.black),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 20,
                       ),
-                      SizedBox(width: 40),
-                      Text("BSIT"),
-                      SizedBox(width: 40),
-                      ElevatedButton(onPressed: () {}, child: Text("Edit")),
-                    ],
-                  ),
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          topRight: Radius.circular(0),
+                          bottomLeft: Radius.circular(20),
+                          bottomRight: Radius.circular(40),
+                        ),
+                      ),
+                      child: Text(
+                        "Full Name",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 40),
+                    Text("Di O. Nela"),
+                    SizedBox(width: 40),
+                    ElevatedButton(onPressed: () {}, child: Icon(Icons.edit)),
+                  ],
                 ),
               ),
+            ),
 
-              ListTile(
-                title: Container(
-                  padding: EdgeInsets.all(0),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(40),
-                    border: Border.all(color: Colors.black),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          "Section",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                        decoration: BoxDecoration(color: Colors.blue),
+            ListTile(
+              title: Container(
+                padding: EdgeInsets.all(0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(40),
+                  border: Border.all(color: Colors.black),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 20,
                       ),
-                      SizedBox(width: 40),
-                      Text("2-1A"),
-                      SizedBox(width: 40),
-                      ElevatedButton(onPressed: () {}, child: Text("Edit")),
-                    ],
-                  ),
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          topRight: Radius.circular(0),
+                          bottomLeft: Radius.circular(20),
+                          bottomRight: Radius.circular(40),
+                        ),
+                      ),
+
+                      child: Text(
+                        "Program",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 40),
+                    Text("BSIT"),
+                    SizedBox(width: 40),
+                    ElevatedButton(onPressed: () {}, child: Icon(Icons.edit)),
+                  ],
                 ),
               ),
+            ),
 
-              //Interest
-              ListTile(
-                title: Container(
-                  padding: EdgeInsets.all(0),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
-                    border: Border.all(color: Colors.black),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          "Interest",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                        decoration: BoxDecoration(color: Colors.blue),
+            ListTile(
+              title: Container(
+                padding: EdgeInsets.all(0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(40),
+                  border: Border.all(color: Colors.black),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 20,
                       ),
-                      SizedBox(width: 40),
-                      Text("Ethanol"),
-                      Text("Ikimandos"),
-                      Text("Marilag"),
-                      SizedBox(width: 40),
-                      ElevatedButton(onPressed: () {}, child: Text("Edit")),
-                    ],
-                  ),
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          topRight: Radius.circular(0),
+                          bottomLeft: Radius.circular(20),
+                          bottomRight: Radius.circular(40),
+                        ),
+                      ),
+
+                      child: Text(
+                        "Section",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 40),
+                    Text("2-1A"),
+                    SizedBox(width: 40),
+                    ElevatedButton(onPressed: () {}, child: Icon(Icons.edit)),
+                  ],
                 ),
               ),
-              ListTile(
-                title: Container(
-                  padding: EdgeInsets.all(0),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
-                    border: Border.all(color: Colors.black),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          "Recent Activities",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
+            ),
+
+            //Interest
+            ListTile(
+              title: Container(
+                padding: EdgeInsets.all(0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(color: Colors.black),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.symmetric(
+                            vertical: 10,
+                            horizontal: 20,
+                          ),
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(20),
+                              topRight: Radius.circular(0),
+                              bottomLeft: Radius.circular(0),
+                              bottomRight: Radius.circular(40),
+                            ),
+                          ),
+                          child: Text(
+                            "Interest",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
                           ),
                         ),
-                        decoration: BoxDecoration(color: Colors.blue),
+
+                        ElevatedButton(onPressed: () {}, child: Text("Edit")),
+                      ],
+                    ),
+                    SizedBox(width: 40),
+                    //put in container
+                    Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Ethanol"),
+                          Text("Ikimandians"),
+                          Text("Marilag"),
+                          SizedBox(width: 40),
+                        ],
                       ),
-                      SizedBox(width: 40),
-                      Text(
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            ListTile(
+              title: Container(
+                padding: EdgeInsets.all(0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(color: Colors.black),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 20,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          topRight: Radius.circular(0),
+                          bottomLeft: Radius.circular(00),
+                          bottomRight: Radius.circular(40),
+                        ),
+                      ),
+                      child: Text(
+                        "Recent Activities",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 40),
+                    Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Text(
                         "03/25/2025 | Attended URS Binangonan College Week: March...",
                       ),
-                      SizedBox(width: 40),
-                    ],
-                  ),
+                    ),
+                    SizedBox(width: 40),
+                  ],
                 ),
               ),
-              ElevatedButton(onPressed: () {}, child: Text("Save Profile")),
-            ],
-          ),
+            ),
+            ElevatedButton(onPressed: () {}, child: Text("Save Profile")),
+          ],
         ),
-
+      ),
     );
   }
 }
